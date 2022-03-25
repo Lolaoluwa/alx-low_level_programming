3eb3db62f7debd7e808c535131313b5e2961f363
Lolaoluwa <oghovomare2000@gmail.com>
2022-03-25 01:34:53 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/main.c
@@ -0,0 +1,59 @@
+
+You have a captain's log due before 2022-03-27 (in 2 days)! Log it now!
+0x06. C - More pointers, arrays and strings
+ By Julien Barbier
+ Weight: 1
+ Ongoing second chance project - started 03-23-2022, must end by 03-26-2022 (in about 21 hours) - you're done with 0% of tasks.
+ An auto review will be launched at the deadline
+
+
+Requirements
+General
+Allowed editors: vi, vim, emacs
+All your files will be compiled on Ubuntu 20.04 LTS using gcc, using the options -Wall -Werror -Wextra -pedantic -std=gnu89
+All your files should end with a new line
+A README.md file, at the root of the folder of the project is mandatory
+Your code should use the Betty style. It will be checked using betty-style.pl and betty-doc.pl
+You are not allowed to use global variables
+No more than 5 functions per file
+You are not allowed to use the standard library. Any use of functions like printf, puts, etc… is forbidden
+You are allowed to use _putchar
+You don’t have to push _putchar.c, we will use our file. If you do it won’t be taken into account
+In the following examples, the main.c files are shown as examples. You can use them to test your functions, but you don’t have to push them to your repo (if you do we won’t take them into account). We will use our own main.c files at compilation. Our main.c files might be different from the one shown in the examples
+The prototypes of all your functions and the prototype of the function _putchar should be included in your header file called main.h
+Don’t forget to push your header file
+Quiz questions
+Great! You've completed the quiz successfully! Keep going! (Show quiz)
+Tasks
+0. strcat
+mandatory
+Write a function that concatenates two strings.
+
+Prototype: char *_strcat(char *dest, char *src);
+This function appends the src string to the dest string, overwriting the terminating null byte (\0) at the end of dest, and then adds a terminating null byte
+Returns a pointer to the resulting string dest
+FYI: The standard library provides a similar function: strcat. Run man strcat to learn more.
+
+julien@ubuntu:~/0x06$ cat 0-main.c
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
+
+    printf("%s\n", s1);
+    printf("%s", s2);
+    ptr = _strcat(s1, s2);
+    printf("%s", s1);
+    printf("%s", s2);
+    printf("%s", ptr);
+    return (0);
+}
