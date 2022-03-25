6cd70feec15ca63c5ff1dc5f179d418e1f5f7211
Lolaoluwa <oghovomare2000@gmail.com>
2022-03-25 14:46:51 -0700

    advanced

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,35 +1,53 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * print_number - printing number with puts
+ * infinite_add -  adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: result
+ * @size_r: result lenght
+ * Return: sum
  *
- * @n: input int
  */
-void print_number(int n)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+
 {
-	/*Declaring variables*/
-	int count = 0, pow = 1;
-	unsigned int num = n;
+/* local variable declaration */
+	int i = 0, j = 0, k, l = 0, f, s, d = 0;
 
-
-	if (n < 0) /*Evaluate this condition*/
+	while (n1[i] != '\0')
+		i++;
+	while (n2[j] != '\0')
+		j++;
+	if (i > j)
+		l = i;
+	else
+		l = j;
+	if (l + 1 > size_r)
+		return (0);
+	r[l] = '\0';
+	for (k = l - 1 ; k >= 0 ; k--)
 	{
-		_putchar('-');
-		num = -n;
+		i--;
+		j--;
+		if (i >= 0)
+			f = n1[i] - '0';
+		else
+			f = 0;
+		if (j >= 0)
+			s = n2[j] - '0';
+		else
+			s = 0;
+		r[k] = (f + s + d) % 10 + '0';
+		d = (f + s + d) / 10;
 	}
-	while (n != 0)
+	if (d == 1)
 	{
-		n /= 10;
-		count++;
+		r[l + 1] = '\0';
+		if (l + 2 > size_r)
+			return (0);
+		while (l-- >= 0)
+			r[l + 1] = r[l];
+		r[0] = d + '0';
 	}
-	while (count > 1)
-	{
-		pow *= 10;
-		count--;
-	}
-	while (pow >= 1)
-	{
-		_putchar(num / pow % 10 + '0');
-		pow /= 10;
-	}
+	return (r);
 }
