2a53f99fa790876183ef84ab7827233f76b855a3
Lolaoluwa <oghovomare2000@gmail.com>
2022-05-05 12:08:44 -0700

    rrd txtfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,29 +14,28 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-  int fd, res_read, res_write;
-  char *buf;
-
-  if (filename == NULL)
-    return (0);
-  fd = open(filename, O_RDONLY);
-  if (fd == -1)
-    return (0);
-  buf = malloc(sizeof(char) * letters);
-  if (buf == NULL)
-    return (0);
-  res_read = read(fd, buf, letters);
-  if (res_read == -1)
-    {
-      free(buf);
-      return (0);
-    }
-  res_write = write(STDOUT_FILENO, buf, res_read);
-  if (res_write == -1 || res_read != res_write)
-    {
-      free(buf);
-      return (0);
-    }
+int fd, res_read, res_write;
+char *buf;
+if (filename == NULL)
+return (0);
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
+buf = malloc(sizeof(char) * letters);
+if (buf == NULL)
+return (0);
+res_read = read(fd, buf, letters);
+if (res_read == -1)
+{
+free(buf);
+return (0);
+}
+res_write = write(STDOUT_FILENO, buf, res_read);
+if (res_write == -1 || res_read != res_write)
+{
+free(buf);
+return (0);
+}
 free(buf);
 close(fd);
 return (res_write);
